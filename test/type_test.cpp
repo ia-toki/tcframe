c832d972c365fa3e260b05aec1e157e5fde31afb
Ashar Fuadi <fushar@gmail.com>
2015-02-17 21:40:58 +0700

    Add unit tests for horizontal and vertical vectors
    For #2

--- test/type_test.cpp
+++ test/type_test.cpp
@@ -5,8 +5,10 @@
 #include <sstream>
 
 using std::ostringstream;
+using tcframe::HorizontalVector;
 using tcframe::Matrix;
 using tcframe::Scalar;
+using tcframe::VerticalVector;
 
 TEST(ScalarTest, Printing) {
     int x;
@@ -17,6 +19,33 @@ TEST(ScalarTest, Printing) {
     wrappedX.printTo(sout);
 
     EXPECT_EQ("42", sout.str());
+}
+
+TEST(HorizontalVectorTest, Printing) {
+    vector<int> V;
+    HorizontalVector<int> wrappedV(V);
+
+    V = vector<int>{1, 2, 3};
+    ostringstream sout;
+    wrappedV.printTo(sout);
+
+    EXPECT_EQ("1 2 3", sout.str());
+}
+
+TEST(VerticalVectorTest, Printing) {
+    vector<int> V;
+    VerticalVector<int> wrappedV(V);
+
+    V = vector<int>{1, 2, 3};
+    ostringstream sout;
+    wrappedV.printElementTo(0, sout);
+    sout << "\n";
+    wrappedV.printElementTo(1, sout);
+    sout << "\n";
+    wrappedV.printElementTo(2, sout);
+    sout << "\n";
+
+    EXPECT_EQ("1\n2\n3\n", sout.str());
 }
 
 TEST(MatrixTest, CharPrinting) {
