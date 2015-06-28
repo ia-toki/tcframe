b198448174f1a589f504d4931d9bfa492403d6ca
Ashar Fuadi <fushar@gmail.com>
2015-06-28 15:38:22 +0700

    Add Problem's command-line options tests

--- test/problem_test.cpp
+++ test/problem_test.cpp
@@ -34,3 +34,28 @@ TEST(ProblemTest, MyOptions) {
     EXPECT_EQ(2, problem->getTimeLimit());
     EXPECT_EQ(256, problem->getMemoryLimit());
 }
+
+TEST(ProblemTest, CommandLineOptions) {
+    char* argv[] = {(char*) "<runner>", (char*)"--slug=bar", (char*)"--time-limit=3", (char*)"--memory-limit=64"};
+
+    BaseProblem* problem = new MyProblem();
+    problem->applyProblemConfiguration();
+    problem->applyProblemCommandLineOptions(4, argv);
+
+    EXPECT_EQ("bar", problem->getSlug());
+    EXPECT_EQ(3, problem->getTimeLimit());
+    EXPECT_EQ(64, problem->getMemoryLimit());
+}
+
+
+TEST(ProblemTest, CommandLineOptionsWithNos) {
+    char* argv[] = {(char*) "<runner>", (char*)"--no-time-limit", (char*)"--no-memory-limit"};
+
+    BaseProblem* problem = new MyProblem();
+    problem->applyProblemConfiguration();
+    problem->applyProblemCommandLineOptions(3, argv);
+
+    EXPECT_EQ("foo", problem->getSlug());
+    EXPECT_EQ(0, problem->getTimeLimit());
+    EXPECT_EQ(0, problem->getMemoryLimit());
+}
