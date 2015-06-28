d4143021bc9da4664fadc2561a089cbfc903dcfd
Ashar Fuadi <fushar@gmail.com>
2015-06-28 20:56:20 +0700

    Tidy up generator tests

--- test/generator_test.cpp
+++ test/generator_test.cpp
@@ -37,45 +37,45 @@ TEST(GeneratorTest, CommandLineOptions) {
 }
 
 TEST(GeneratorTest, GenerationWithSubtasksAndTestGroups) {
-    FakeGeneratorLogger logger;
-    GeneratorWithTestGroups gen(&logger, new FakeGeneratorOperatingSystem());
-    int exitCode = gen.generate();
+    FakeGeneratorLogger* logger = new FakeGeneratorLogger();
+    BaseGenerator<ProblemWithSubtasks>* generator = new GeneratorWithTestGroups(logger, new FakeGeneratorOperatingSystem());
+    int exitCode = generator->generate();
 
     EXPECT_EQ(0, exitCode);
 
-    EXPECT_EQ(0, logger.getFailures("problem_sample_1").size());
-    EXPECT_EQ(0, logger.getFailures("problem_1_1").size());
-    EXPECT_EQ(0, logger.getFailures("problem_1_2").size());
-    EXPECT_EQ(0, logger.getFailures("problem_2_1").size());
-    EXPECT_EQ(0, logger.getFailures("problem_2_2").size());
-    EXPECT_EQ(0, logger.getFailures("problem_3_1").size());
+    EXPECT_EQ(0, logger->getFailures("problem_sample_1").size());
+    EXPECT_EQ(0, logger->getFailures("problem_1_1").size());
+    EXPECT_EQ(0, logger->getFailures("problem_1_2").size());
+    EXPECT_EQ(0, logger->getFailures("problem_2_1").size());
+    EXPECT_EQ(0, logger->getFailures("problem_2_2").size());
+    EXPECT_EQ(0, logger->getFailures("problem_3_1").size());
 }
 
 TEST(GeneratorTest, FailedGenerationWithSubtasksAndTestGroups) {
-    FakeGeneratorLogger logger;
-    InvalidGeneratorWithTestGroups gen(&logger, new FakeGeneratorOperatingSystem());
-    int exitCode = gen.generate();
+    FakeGeneratorLogger* logger = new FakeGeneratorLogger();
+    BaseGenerator<ProblemWithSubtasks>* generator = new InvalidGeneratorWithTestGroups(logger, new FakeGeneratorOperatingSystem());
+    int exitCode = generator->generate();
 
     EXPECT_NE(0, exitCode);
 
-    auto failures_sample_1 = logger.getFailures("problem_sample_1");
+    auto failures_sample_1 = logger->getFailures("problem_sample_1");
     ASSERT_EQ(2, failures_sample_1.size());
     EXPECT_EQ(Failure("Does not satisfy subtask 1, on constraints:", 0), failures_sample_1[0]);
     EXPECT_EQ(Failure("1 <= B && B <= 1000", 1), failures_sample_1[1]);
 
-    auto failures_1_1 = logger.getFailures("problem_1_1");
+    auto failures_1_1 = logger->getFailures("problem_1_1");
     ASSERT_EQ(0, failures_1_1.size());
 
-    auto failures_1_2 = logger.getFailures("problem_1_2");
+    auto failures_1_2 = logger->getFailures("problem_1_2");
     ASSERT_EQ(2, failures_1_2.size());
     EXPECT_EQ(Failure("Does not satisfy subtask 1, on constraints:", 0), failures_1_2[0]);
     EXPECT_EQ(Failure("1 <= B && B <= 1000", 1), failures_1_2[1]);
 
-    auto failures_2_1 = logger.getFailures("problem_2_1");
+    auto failures_2_1 = logger->getFailures("problem_2_1");
     ASSERT_EQ(1, failures_2_1.size());
     EXPECT_EQ(Failure("Satisfies subtask 1 but is not assigned to it", 0), failures_2_1[0]);
 
-    auto failures_2_2 = logger.getFailures("problem_2_2");
+    auto failures_2_2 = logger->getFailures("problem_2_2");
     ASSERT_EQ(4, failures_2_2.size());
     EXPECT_EQ(Failure("Does not satisfy subtask 2, on constraints:", 0), failures_2_2[0]);
     EXPECT_EQ(Failure("1 <= A && A <= 2000000000", 1), failures_2_2[1]);
@@ -84,50 +84,55 @@ TEST(GeneratorTest, FailedGenerationWithSubtasksAndTestGroups) {
 }
 
 TEST(GeneratorTest, GenerationWithoutSubtasksAndWithoutTestGroups) {
-    FakeGeneratorLogger logger;
-    GeneratorWithoutTestGroups gen(&logger, new FakeGeneratorOperatingSystem());
-    int exitCode = gen.generate();
+    FakeGeneratorLogger* logger = new FakeGeneratorLogger();
+    BaseGenerator<ProblemWithoutSubtasks>* generator = new GeneratorWithoutTestGroups(logger, new FakeGeneratorOperatingSystem());
+    int exitCode = generator->generate();
 
     EXPECT_EQ(0, exitCode);
 
-    EXPECT_EQ(0, logger.getFailures("problem_sample_1").size());
-    EXPECT_EQ(0, logger.getFailures("problem_sample_2").size());
-    EXPECT_EQ(0, logger.getFailures("problem_1").size());
-    EXPECT_EQ(0, logger.getFailures("problem_2").size());
+    EXPECT_EQ(0, logger->getFailures("problem_sample_1").size());
+    EXPECT_EQ(0, logger->getFailures("problem_sample_2").size());
+    EXPECT_EQ(0, logger->getFailures("problem_1").size());
+    EXPECT_EQ(0, logger->getFailures("problem_2").size());
 }
 
 TEST(GeneratorTest, FailedGenerationWithoutSubtasksAndWithoutTestGroups) {
-    FakeGeneratorLogger logger;
-    InvalidGeneratorWithoutTestGroups gen(&logger, new FakeGeneratorOperatingSystem());
-    int exitCode = gen.generate();
+    FakeGeneratorLogger* logger = new FakeGeneratorLogger();
+    BaseGenerator<ProblemWithoutSubtasks>* generator = new InvalidGeneratorWithoutTestGroups(logger, new FakeGeneratorOperatingSystem());
+    int exitCode = generator->generate();
 
     EXPECT_NE(0, exitCode);
 
-    auto failures_sample_1 = logger.getFailures("problem_sample_1");
+    auto failures_sample_1 = logger->getFailures("problem_sample_1");
     ASSERT_EQ(0, failures_sample_1.size());
 
-    auto failures_sample_2 = logger.getFailures("problem_sample_2");
+    auto failures_sample_2 = logger->getFailures("problem_sample_2");
     ASSERT_EQ(2, failures_sample_2.size());
     EXPECT_EQ(Failure("Does not satisfy constraints, on:", 0), failures_sample_2[0]);
     EXPECT_EQ(Failure("0 <= K <= 100", 1), failures_sample_2[1]);
 
-    auto failures_1 = logger.getFailures("problem_1");
+    auto failures_1 = logger->getFailures("problem_1");
     ASSERT_EQ(0, failures_1.size());
 
-    auto failures_2 = logger.getFailures("problem_2");
+    auto failures_2 = logger->getFailures("problem_2");
     ASSERT_EQ(2, failures_sample_2.size());
     EXPECT_EQ(Failure("Does not satisfy constraints, on:", 0), failures_2[0]);
     EXPECT_EQ(Failure("1 <= B && B <= 1000", 1), failures_2[1]);
 }
 
 TEST(GeneratorTest, GenerationWithFailedExecution) {
-    FakeGeneratorLogger logger;
-    GeneratorWithoutTestGroups gen(&logger, new FakeGeneratorOperatingSystem({ {"problem_1-generation-evaluation", ExecutionResult{1, new istringstream(), new istringstream("Intentionally failed")} } }));
-    int exitCode = gen.generate();
+    FakeGeneratorLogger* logger = new FakeGeneratorLogger();
+
+    map<string, ExecutionResult> arrangedResultsMap = {
+            {"problem_1-generation-evaluation", ExecutionResult{1, new istringstream(), new istringstream("Intentionally failed")} }
+    };
+
+    BaseGenerator<ProblemWithoutSubtasks>* generator = new GeneratorWithoutTestGroups(logger, new FakeGeneratorOperatingSystem(arrangedResultsMap));
+    int exitCode = generator->generate();
 
     EXPECT_NE(0, exitCode);
 
-    auto failures = logger.getFailures("problem_1");
+    auto failures = logger->getFailures("problem_1");
     ASSERT_EQ(3, failures.size());
     EXPECT_EQ(Failure("Execution of solution failed:", 0), failures[0]);
     EXPECT_EQ(Failure("Exit code: 1", 1), failures[1]);
