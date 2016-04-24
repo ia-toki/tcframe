ab15a7cad4140954a4a4a946ff7d7fc65d005363
Ashar Fuadi <fushar@gmail.com>
2016-04-24 23:35:37 +0100

    Use safer cast from const char* literal to char*

--- test/tcframe/runner/RunnerTests.cpp
+++ test/tcframe/runner/RunnerTests.cpp
@@ -16,9 +16,16 @@ class FakeGenerator : public BaseGenerator<FakeProblem> {};
 class RunnerTests : public Test {
 protected:
     int argc = 1;
-    char* argv[1] = {const_cast<char*>("./runner")};
+    char* argv[1] = {toChar("./runner")};
 
     Runner<FakeProblem> runner = Runner<FakeProblem>(argc, argv);
+
+private:
+    static char* toChar(const string& str) {
+        char* cstr = new char[str.length() + 1];
+        strcpy(cstr, str.c_str());
+        return cstr;
+    }
 };
 
 TEST_F(RunnerTests, CompilationSuccessful) {
