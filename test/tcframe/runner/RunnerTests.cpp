1974c2aa575ea18c219726af138a352f40ceba96
Ashar Fuadi <fushar@gmail.com>
2016-04-27 00:46:07 +0100

    Fix memory leak in RunnerTests

--- test/tcframe/runner/RunnerTests.cpp
+++ test/tcframe/runner/RunnerTests.cpp
@@ -16,20 +16,14 @@ class FakeGenerator : public BaseGenerator<FakeProblem> {};
 class RunnerTests : public Test {
 protected:
     int argc = 1;
-    char* argv[1] = {toChar("./runner")};
+    char* argv[1] = {(char*) "./runner"};
 
     Runner<FakeProblem> runner = Runner<FakeProblem>(argc, argv);
-
-private:
-    static char* toChar(const string& str) {
-        char* cstr = new char[str.length() + 1];
-        strcpy(cstr, str.c_str());
-        return cstr;
-    }
 };
 
 TEST_F(RunnerTests, CompilationSuccessful) {
-    runner.setGenerator(new FakeGenerator());
+    FakeGenerator generator;
+    runner.setGenerator(&generator);
 }
 
 }
