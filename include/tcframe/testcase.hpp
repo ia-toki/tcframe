999c8b00ac259fcd862fd5665322b8b9bc5c3cc9
Ashar Fuadi <fushar@gmail.com>
2016-04-30 12:35:19 +0100

    oops

--- include/tcframe/testcase.hpp
+++ include/tcframe/testcase.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
 #include "tcframe/testcase/OfficialTestCase.hpp"
+#include "tcframe/testcase/SampleTestCase.hpp"
 #include "tcframe/testcase/TestGroup.hpp"
 #include "tcframe/testcase/TestSuite.hpp"
