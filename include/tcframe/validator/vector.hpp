c9aa2529a0c7e2250d7f2c325626048404108b91
JoelGunawan <joelgun14@gmail.com>
2024-10-16 22:04:48 -0700

    added elementsAreNonAscending(), elementsAreDescending(), and elementsAreAscending() validators

--- include/tcframe/validator/vector.hpp
+++ include/tcframe/validator/vector.hpp
@@ -24,4 +24,44 @@ bool elementsAreNonDescending(const vector<T>& v) {
     return true;
 }
 
+template<typename T>
+bool elementsAreNonAscending(const vector<T> &v) {
+    for (std::size_t i = 1; i < v.size(); ++i) {
+        if (v[i - 1] < v[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+template<typename T>
+bool elementsAreDescending(const vector<T>& v) {
+    for (std::size_t i = 1; i < v.size(); ++i) {
+        if (v[i - 1] <= v[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+template<typename T>
+bool elementsAreAscending(const vector<T> &v) {
+    for (std::size_t i = 1; i < v.size(); ++i) {
+        if (v[i - 1] >= v[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+template<typename T, typename Compare>
+bool elementsAreOrdered(const vector<T> &v, Compare comp) {
+    for (std::size_t i = 1; i < v.size(); ++i) {
+        if(!comp(v[i - 1], v[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
 }
