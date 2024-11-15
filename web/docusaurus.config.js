364850205049591857c88cf7aa47746e7ac9e9ce
Ashar Fuadi <fushar@gmail.com>
2024-11-15 20:52:54 +0700

    Fix doc slogan

--- web/docusaurus.config.js
+++ web/docusaurus.config.js
@@ -9,7 +9,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'TCFrame',
-  tagline: 'A test generation framework for competitive programming problems.',
+  tagline: 'A test case generation framework for competitive programming problems.',
   favicon: 'img/logo.png',
 
   // Set the production url of your site here
